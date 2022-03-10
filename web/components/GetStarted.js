0f129b534a0bb4f7f23dfc6edab5879f63673210
shroep <100433752+shroep@users.noreply.github.com>
2022-03-09 21:06:18 -0500

    v1.3

--- web/components/GetStarted.js
+++ web/components/GetStarted.js
@@ -2,7 +2,7 @@ import { Container, Grid } from '@mui/material';
 import styles from '../styles/Home.module.css';
 import { Button, Card, CardActions, CardContent, Input, Box, Typography } from '@mui/material';
 import Image from 'next/image';
-import profilePic from '/public/draft-3.png'
+import profilePic from '/public/sample-nft.png'
 
 const GetStarted = () => {
   return (
